a7aaf78021e80d788d061579b6e066d84d1e847b
WJ <467001580@qq.com>
2016-01-18 20:27:22 +0800

    加入显示天气信息功能.

---
+++ app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,146 @@
+package com.coolweather.app.activity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.coolweather.app.R;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+/**
+ * Created by WJ on 2016/1/18.
+ */
+public class WeatherActivity extends Activity {
+    private LinearLayout weatherInfoLayout;
+    /**
+     * 用户显示城市名
+     */
+    private TextView cityNameText;
+    /**
+     * 用户显示发布时间
+     */
+    private TextView publishText;
+    /**
+     * 用于显示天气描述信息
+     */
+    private TextView weatherDespText;
+    /**
+     * 用于显示气温1
+     */
+    private TextView temp1Text;
+    /**
+     * 用于显示气温2
+     */
+    private TextView temp2Text;
+    /**
+     * 用于显示当前日期
+     */
+    private TextView currentDateText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+        //初始化各控件
+        weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView) findViewById(R.id.city_name);
+        publishText = (TextView) findViewById(R.id.publish_text);
+        weatherDespText = (TextView) findViewById(R.id.weather_desp);
+        temp1Text = (TextView) findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDateText = (TextView) findViewById(R.id.current_date);
+
+        String countyCode = getIntent().getStringExtra("county_code");
+        if (!TextUtils.isEmpty(countyCode)) {
+            //有县级代号时就去查询天气
+            publishText.setText("同步中...");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countyCode);
+        } else {
+            //没有县级代号就直接显示本地天气
+            showWeather();
+        }
+    }
+
+    /**
+     * 查询县级代号所对应的天气代号
+     *
+     * @param countyCode
+     */
+    private void queryWeatherCode(String countyCode) {
+        String address = "http://www.weather.com.cn/data/list3/city" + countyCode + ".xml";
+        queryFromService(address, "countyCode");
+    }
+
+    /**
+     * 查询天气代号所对应的天气
+     *
+     * @param weatherCode
+     */
+    private void queryWeatherInfo(String weatherCode) {
+        String address = "http://www.weather.com.cn/adat/cityinfo/" + weatherCode + ".html";
+        queryFromService(address, "weatherCode");
+    }
+
+    private void queryFromService(final String address, final String type) {
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                if ("countyCode".equals(type)) {
+                    if (!TextUtils.isEmpty(response)) {
+                        //从服务器返回数据中解析出天气代号
+                        String[] array = response.split("\\|");
+                        if (array != null && array.length == 2) {
+                            String weatherCode = array[1];
+                            queryWeatherInfo(weatherCode);
+                        }
+                    }
+                } else if ("weatherCode".equals(type)) {
+                    //处理服务器返回的天气信息
+                    Utility.handleWeatherResponse(WeatherActivity.this, response);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showWeather();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onErroe(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishText.setText("同步失败");
+                    }
+                });
+            }
+        });
+    }
+    /**
+     * 从SharedPreferences文件中读取存储天气信息，并显示到界面上
+     */
+    private void showWeather(){
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText(prefs.getString("city_name",""));
+        temp1Text.setText(prefs.getString("temp1",""));
+        temp2Text.setText(prefs.getString("temp2",""));
+        weatherDespText.setText(prefs.getString("weather_desp",""));
+        publishText.setText("今天"+prefs.getString("publish_time","")+"发布");
+        currentDateText.setText(prefs.getString("current_date",""));
+        weatherInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+    }
+}
+
--- app/src/main/java/com/coolweather/app/util/Utility.java
+++ app/src/main/java/com/coolweather/app/util/Utility.java
@@ -1,11 +1,20 @@
 package com.coolweather.app.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.coolweather.app.db.CoolWeatherDB;
 import com.coolweather.app.model.City;
 import com.coolweather.app.model.County;
 import com.coolweather.app.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by WJ on 2016/1/18.
@@ -51,16 +60,17 @@ public class Utility {
         }
         return false;
     }
+
     /**
      * 解析和处理服务器返回的县级数据
      */
-    public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,String response,int cityId){
-        if (!TextUtils.isEmpty(response)){
-            String[] allCounties=response.split(",");
-            if (allCounties!=null&&allCounties.length>0){
-                for (String c:allCounties){
-                    String[] array=c.split("\\|");
-                    County county=new County();
+    public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB, String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCounties = response.split(",");
+            if (allCounties != null && allCounties.length > 0) {
+                for (String c : allCounties) {
+                    String[] array = c.split("\\|");
+                    County county = new County();
                     county.setCountyCode(array[0]);
                     county.setCountyName(array[1]);
                     county.setCityId(cityId);
@@ -70,5 +80,49 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 解析服务器返回的JSON数据，并将解析出的数据存储到本地
+     */
+    public static void handleWeatherResponse(Context context, String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName=weatherInfo.getString("city");
+            String weatherCode=weatherInfo.getString("cityid");
+            String temp1=weatherInfo.getString("temp1");
+            String temp2=weatherInfo.getString("temp2");
+            String weatherDesp=weatherInfo.getString("weather");
+            String publishTime=weatherInfo.getString("ptime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中。
+     * @param context
+     * @param cityName
+     * @param weatherCode
+     * @param temp1
+     * @param temp2
+     * @param weatherDesp
+     * @param publishTime
+     */
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1, String temp2, String weatherDesp, String publishTime) {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日");
+        SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
     }
 }
